88aff43e36ab4cadd0629185283dba2e64bf963b
UndiFineD <keimpe@example.com>
2026-03-03 07:51:10 +0000

    auto fix basic syntactic cleanups

--- src/classes/orchestration/ImmunizationOrchestrator.py
+++ src/classes/orchestration/ImmunizationOrchestrator.py
@@ -6,7 +6,7 @@ import re
 from typing import Dict, List, Any, Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
-from src.classes.fleet.FleetManager import FleetManager
+    from src.classes.fleet.FleetManager import FleetManager
 
 class ImmunizationOrchestrator:
     """
