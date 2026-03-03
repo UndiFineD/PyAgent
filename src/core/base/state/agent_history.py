a781af1a0175f15500cf4998edb09ec4fb5b3c17
UndiFineD <keimpe@example.com>
2026-03-03 06:55:27 +0000

    auto fix basic syntactic cleanups

--- src/core/base/state/agent_history.py
+++ src/core/base/state/agent_history.py
@@ -24,8 +24,8 @@ from typing import TYPE_CHECKING
 from src.core.base.common.models import ConversationMessage, MessageRole
 
 if TYPE_CHECKING:
-    from src.core.base.lifecycle.agent_core import BaseCore
-    from src.core.base.lifecycle.base_agent_core import BaseAgentCore
+from src.core.base.lifecycle.agent_core import BaseCore
+from src.core.base.lifecycle.base_agent_core import BaseAgentCore
 
 
 class AgentConversationHistory:
